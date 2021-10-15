f0d0b1345e46fb73381744876f77d29a25cf7acb
Nabih <estefan.nabih@gmail.com>
2021-10-15 00:47:01 -0400

    rebuilding site Fri 15 Oct 2021 12:47:01 AM EDT

--- deploy.sh
+++ deploy.sh
@@ -19,3 +19,18 @@ git commit -m "$msg"
 git push origin main --recurse-submodules=check
 
 printf "\033[0;32mPushed to Portfolio\033[0m\n"
+
+printf "\033[0;32mDeploying Website Updates to GitHub...\033[0m\n"
+# check message
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+cd public
+#commit to Portfolio repo
+git add -A
+git commit -m "$msg"
+git push origin main --recurse-submodules=check
+
+printf "\033[0;32mPushed to Website\033[0m\n"
