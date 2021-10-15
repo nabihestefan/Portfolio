0853cf33a48349d00c7642892555fe1d48c92927
Nabih <estefan.nabih@gmail.com>
2021-10-14 23:56:19 -0400

    rebuilding site Thu 14 Oct 2021 11:56:19 PM EDT

--- deploy.sh
+++ deploy.sh
@@ -10,31 +10,15 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 
-#commit to Portfolio repo
-git add -A
-git commit -m "$msg"
-git push origin main
-
-printf "\033[0;32mPushed to Portfolio\033[0m\n"
-
 # Build the project.
 hugo -t hugo-resume
 
-## Publish
-
-# If a command fails then the deploy stops
-set -e
-
-printf "\033[0;32mDeploying Public Updates to GitHub...\033[0m\n"
-# check message
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-
 #commit to Portfolio repo
-git add public/.
+git add -A
 git commit -m "$msg"
-git push website main
 
-printf "\033[0;32mPushed to Public\033[0m\n"
+git push --recurse-submodules=on-demand
+
+git push origin main
+
+printf "\033[0;32mPushed to Portfolio\033[0m\n"
